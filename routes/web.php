39c8e46b1bcf46c0986f523bf0ad6e2a68f62936
Simone Stefani <s.stefani95@gmail.com>
2017-05-31 14:39:36 +0200

    Add channel filtering

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,6 @@ Route::get('/home', 'HomeController@index');
 Route::get('/threads', 'ThreadsController@index');
 Route::post('/threads', 'ThreadsController@store');
 Route::get('/threads/create', 'ThreadsController@create');
+Route::get('/threads/{channel}', 'ThreadsController@index');
 Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
