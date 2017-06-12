0f523d66987e1bcad9bef2313a97b31e8100610e
Simone Stefani <s.stefani95@gmail.com>
2017-06-12 08:20:39 +0200

    Add delete thread feature

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -69,7 +69,7 @@ class ThreadsController extends Controller
      * @param  \App\Thread  $thread
      * @return \Illuminate\Http\Response
      */
-    public function show($channelId, Thread $thread)
+    public function show($channel, Thread $thread)
     {
         return view('threads.show', [
             'thread' => $thread,
@@ -106,9 +106,15 @@ class ThreadsController extends Controller
      * @param  \App\Thread  $thread
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Thread $thread)
+    public function destroy($channel, Thread $thread)
     {
-        //
+        $thread->delete();
+
+        if(request()->wantsJson()) {
+            return response([], 204); 
+        }
+
+        return redirect('/threads');
     }
 
     protected function getThreads($channel, $filters)
